c8bfc4fb2edbe724e5863fd2a2d11bab8a594d7a
Nirigoyen <116807507+Nirigoyen@users.noreply.github.com>
2023-06-24 15:22:53 -0300

    Changes by timoteo

--- src/Model/Prestamo.java
+++ src/Model/Prestamo.java
@@ -19,11 +19,6 @@ public class Prestamo {
     }
 
 
-    //calcularFechaDevolucion(in fechaInicio:LocalDateTIme, in diasPrestamo:int): fechaDevolucion
-    public void calcularFechaDevolucion(){
-        this.fechaDevolucion = this.fechaDevolucion.plusDays(diasPrestamo);
-    }
-
     //notificarProximoVencimiento(in medio:MediosContacto)
     public void notificarProximoVencimiento(){
        // System.out.println(socio.getMedioPreferido().toString() + ", el proximo vencimiento es: ",calcularFechaDevolucion());
